6d3fd706e44ebaafe7a5ca44db1fe402b30b0ab1
KomoriDev <mute231010@gmail.com>
2025-07-15 14:06:12 +0800

    :sparkles: enhance site header with dynamic title

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,8 +1,30 @@
+"use client"
+
+import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
+import { usePathname } from "next/navigation"
 
 export function SiteHeader() {
+  const pathname = usePathname()
+
+  const getHeaderTitle = (path: string) => {
+    console.log(pathname)
+    switch (path.split("/")[1]) {
+      case "":
+        return "Home"
+      case "auth":
+        return "Auth"
+      case "scenario":
+        return "Chat"
+      default:
+        return "Home"
+    }
+  }
+
+  const title = getHeaderTitle(pathname)
+
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -11,17 +33,17 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">Chat</h1>
+        <h1 className="text-base font-medium">{title}</h1>
         <div className="ml-auto flex items-center gap-2">
           <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
-            <a
-              href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
+            <Link
+              href="https://github.com/KomoriDev/chronos-codex"
               rel="noopener noreferrer"
               target="_blank"
               className="dark:text-foreground"
             >
               GitHub
-            </a>
+            </Link>
           </Button>
         </div>
       </div>
